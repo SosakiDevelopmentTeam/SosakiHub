8ff4d33fea7c56ef3fb6ccd3c61ac268e7c53c41
Canvas123 <canvas123@protonmail.com>
2018-06-16 09:18:58 +0300

    Start function added

--- hub_repos/byond.py
+++ hub_repos/byond.py
@@ -2,9 +2,24 @@ import asyncio
 from asyncio import subprocess
 import os.path as osp
 import os
+from types import *
+
+BUILDING = 0xA
 
 is_win = 0 if os.name == "posix" else 1
 DREAMMAKER = f'"{osp.join(os.environ["BYOND_BIN"], "dm.exe")}"' if is_win else "DreamMaker"
+DREAMDAEMON = f'"{osp.join(os.environ["BYOND_BIN"], "dreamdaemon.exe")}"' if is_win else "DreamDaemon"
+
+
+def dict_to_params(val: dict, l: LambdaType = lambda x: ...) -> str:
+    s = ""
+    if not len(val):
+        return s
+    for key in val:
+        if l(key):
+            s += f"-{key} {val[key]} "
+    return s
+
 
 class BYOND:
 
@@ -13,6 +28,15 @@ class BYOND:
         self.proc = None
 
     async def build(self):
-        proc = await subprocess.create_subprocess_exec(f'{DREAMMAKER} {self.path} -max_errors 10', stdout=subprocess.PIPE)
+        yield {"output": None, "return_code": BUILDING}
+        proc = await subprocess.create_subprocess_exec(f'{DREAMMAKER} {self.path} -max_errors 10',
+                                                       stdout=subprocess.PIPE)
         stdout, stderr = await proc.communiate()
-        return stdout
+        await proc.terminate()
+        yield {"output": stdout, "return_code": proc.returncode}
+
+    async def start(self, parameters: dict = None):
+        build_f = osp.join(osp.dirname(self.path), f"{osp.basename(self.path)[:-4]}.dmb")
+        if osp.isfile(build_f):
+            proc = await subprocess.create_subprocess_exec(f'{DREAMDAEMON} {build_f} {dict_to_params(parameters)}',
+                                                           stdout=subprocess.PIPE)
