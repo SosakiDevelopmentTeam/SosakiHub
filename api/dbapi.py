d10986fb920ffbacc83364e0e32eb5f1b8d375c5
Canvas123 <canvas123@protonmail.com>
2018-06-17 17:39:45 +0300

    DB fixes

--- api/dbapi.py
+++ api/dbapi.py
@@ -4,17 +4,14 @@ import aiosqlite
 class DB:
 
     def __init__(self, db_name):
-        self.database: aiosqlite.Connection = aiosqlite.connect(db_name)
+        self.name = db_name
 
     async def execute(self, command, params=(), commit=True):
-        async with self.database as db:
+        async with aiosqlite.connect(self.name) as db:
             cur = await db.execute(command, params)
             data = [el for el in await cur.fetchall()]
             if commit:
-                await self.database.commit()
+                await db.commit()
             await cur.close()
-
+            await db.close()
             return data
-
-    async def close(self):
-        await self.database.close()
